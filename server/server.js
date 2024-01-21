5dafeb7a7b7a9ca5dbf963964aa8474ab35f2f8e
Uttam <uk3729061@gmail.com>
2024-01-21 13:32:44 +0530

    io config changed

--- server/server.js
+++ server/server.js
@@ -6,15 +6,22 @@ import { log } from "console";
 
 const app = express();
 const httpServer = http.createServer(app);
-const io = new Server(httpServer,{
-    cors:{
-        // origin:"https://muddy-tree-9579.on.fleek.co/",
-        origin:[`*`],
-        // origin:"http://localhost:5173",
-        // origin:"https://master--papaya-platypus-2f50d7.netlify.app/",
-        methods:["GET","PUT"]
-    }
-});
+// const io = new Server(httpServer,{
+//     cors:{
+//         // origin:"https://muddy-tree-9579.on.fleek.co/",
+//         origin:[`*`],
+//         // origin:"http://localhost:5173",
+//         // origin:"https://master--papaya-platypus-2f50d7.netlify.app/",
+//         methods:["GET","PUT"]
+//     }
+// });
+const io = new Server(httpServer, {
+    cors: {
+      origin: true,
+      credentials: true,
+    },
+    allowEIO3: true,
+  });
 
 io.on("connection",(socket)=>{console.log(socket.id)
 
